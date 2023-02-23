68b2ff7b68231478cca4a40757d76da3d0c7d7ae
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-02-23 11:31:08 -0500

    Good template!

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+var App = () => {
+  return <div><h1>Hello!</h1></div>
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env',
+              ['@babel/preset-react', {"runtime": "automatic"}]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+}
