d4ce30bef0ac485c2d0bd2479cd11154e94bfd9c
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-02-26 12:22:17 -0500

    Import redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../reducers/index';
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => { console.log(store.getState()); });
 
 const root = ReactDOM.createRoot(document.querySelector('#app'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
